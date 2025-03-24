7303733aae490ca98d8e1cd35a67e01e21f8e5c6
DrecQ <drecquist@gmail.com>
2025-03-24 10:25:18 +0100

    Utilisation des attributs dynamiques

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <p :style="{color: count  > 5 ? 'red' : 'green' }">
+    Compteur : {{ count }}
+  </p> 
+
+  <div v-show="count >= 5">Bravo, vous avez atteint 5 clics</div>
+    <button @click="increment">Incrémenter</button>
+    <button @click="decrement">Décrémenter</button>
+
+</template>
+
+<script setup>
+
+  import { ref } from "vue";
+  const count = ref(0)
+
+  const increment= () => {
+    count.value++
+  };
+
+  const decrement= () => {
+    count.value--
+  };
+
+</script>
+
